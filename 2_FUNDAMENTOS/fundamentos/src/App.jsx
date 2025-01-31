d3a6a9994682d70d6d0ef4e70dbc81d3f8ba776c
theselegadev <matheus.seleghin7@gmail.com>
2025-01-31 12:17:20 -0300

    Primeiro componente react

---
+++ 2_FUNDAMENTOS/fundamentos/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import FirstComponent from './components/FirstComponent'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Fundamentos React</h1>
+      <FirstComponent/>
+    </div>
+  )
+}
+
+export default App
---
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.jsx
@@ -0,0 +1,9 @@
+const FirstComponent = () => {
+    return (
+        <div>
+            <h1>Meu Primeiro Componente React!</h1>
+        </div>
+    )
+}
+
+export default FirstComponent
