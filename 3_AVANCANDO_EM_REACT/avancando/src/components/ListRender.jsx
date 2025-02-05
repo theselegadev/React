813a58bb075dc3709ed47c03a0dec41b1db3ad73
theselegadev <matheus.seleghin7@gmail.com>
2025-02-05 16:49:53 -0300

    Propriedade key

--- 3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.jsx
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.jsx
@@ -2,11 +2,19 @@ import { useState } from "react"
 
 const ListRender = () => {
     const [list] = useState(["Matheus","Gabriel","Lucas"])
+    const [users] = useState([
+        {id: 1, name: "Matheus", age: 17},
+        {id: 2, name: "João", age: 20},
+        {id: 3, name: "Julia", age: 17}
+    ])
 
     return (
         <div>
             <ul>
-                {list.map((item) => (<li>{item}</li>))}
+                {list.map((item,index) => (<li key={index}>{item}</li>))}
+            </ul>
+            <ul>
+                {users.map((item) => (<li key={item.id}>{item.name}</li>))}
             </ul>
         </div>
     )
