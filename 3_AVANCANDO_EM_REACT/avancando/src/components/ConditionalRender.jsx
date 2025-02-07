11f17d25157f86f234cedd2de1b0f90f2e3844e2
theselegadev <matheus.seleghin7@gmail.com>
2025-02-07 16:15:27 -0300

    If/else no jsx

--- 3_AVANCANDO_EM_REACT/avancando/src/components/ConditionalRender.jsx
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/ConditionalRender.jsx
@@ -2,11 +2,16 @@ import { useState } from "react"
 
 function ConditionalRender() {
     const [x] = useState(false)
+    const [name,setName] = useState("Matheus")
+
   return (
     <div>
         <h2>Isso será exibido?</h2>
-        {x && <p>X é true</p>}
-        {!x && <p>X é false</p>}
+        {x && <p>O x é verdadeiro</p>}
+        {!x && <p>O x é falso</p>}
+        <h2>If ternário</h2>
+        {name === "Matheus" ? (<p>O Nome é matheus</p>) : (<p>O  Nome não é matheus</p>)}
+        <button onClick={()=>setName("João")}>Mudar o nome para João</button>
     </div>
   )
 }
