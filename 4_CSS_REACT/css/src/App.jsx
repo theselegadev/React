f1ecc6c3d593c60d9d88beb883a65c61adea9e03
theselegadev <matheus.seleghin7@gmail.com>
2025-02-22 15:00:29 -0300

    CSS inline

--- 4_CSS_REACT/css/src/App.jsx
+++ 4_CSS_REACT/css/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
       <h1>React com css</h1>
       <MyComponent/>
       <p>Este parágrafo é do app.jsx</p>
+      <p style={{color: "blue", padding: "50px", borderTop: "2px solid red"}}>Este paragráfo é para CSS inline</p>
     </div>
   )
 }
