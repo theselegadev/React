f81db739dd514608f74955646903a812fb839df6
theselegadev <matheus.seleghin7@gmail.com>
2025-02-26 17:01:17 -0300

    CSS inline dinamico

--- 4_CSS_REACT/css/src/App.jsx
+++ 4_CSS_REACT/css/src/App.jsx
@@ -2,12 +2,16 @@ import './App.css'
 import MyComponent from './components/MyComponent'
 
 function App() {
+  const n = 15
+
   return (
     <div className='App'>
       <h1>React com css</h1>
       <MyComponent/>
       <p>Este parágrafo é do app.jsx</p>
       <p style={{color: "blue", padding: "50px", borderTop: "2px solid red"}}>Este paragráfo é para CSS inline</p>
+      <h2 style={n<10 ? {color: "purple"} : {color: "pink"}}>Css dinâmico</h2>
+      <h2 style={n>10 ? {color: "purple"} : {color: "pink"}}>Css dinâmico</h2>
     </div>
   )
 }
